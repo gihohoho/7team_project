edd70785c1da3215b44df2b9833aa4b4dbb3a0e0
hjzheld <dusen7774@naver.com>
2023-09-08 19:40:52 +0900

    bucket 앱 추가

---
+++ bucket/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Bucket(models.Model):
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ bucket/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def bucket(request):
+    pass
+
+def mypage(request):
+    pass
