90ea91e28e8855a9655643cf6e4f90a4087cae44
Kim-Minjae9872 <141595332+Kim-Minjae9872@users.noreply.github.com>
2023-09-09 13:19:34 +0900

    회원가입 페이지 및 기능 추가

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from users.models import User
 
 
 def signup(request):
-    return HttpResponse('signup!!')
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method == 'POST':
+        User.objects.create_user(
+            email=request.POST['email'],
+            username=request.POST['nickname'],
+            password=request.POST['password'],
+        )
+        return redirect('/users/login/')
+    else:
+        return HttpResponse('Invalid request method', status=405)
 
 
 def login(request):
