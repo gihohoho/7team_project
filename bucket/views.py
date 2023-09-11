999280d9f5496313ec698a8213680ca142530147
hjzheld <dusen7774@naver.com>
2023-09-11 10:28:30 +0900

    bucket, mypage 수정

--- bucket/views.py
+++ bucket/views.py
@@ -1,13 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from bucket.models import Bucket
 
+# 메인페이지
 def bucket(request):
-    pass
+    if request.method == "GET":
+        buckets = Bucket.objects.all()
+        context ={
+            "buckets":buckets,
+        }
+        return render(request, "bucket.html", context)
 
-def mypage(request):
-    pass
+# 개인페이지
+@login_required(login_url='/user/login/')    
+@csrf_exempt 
+def mytodo(request):
+    if request.method == "GET":
+        buckets = Bucket.objects.all()
+        buckets_list = buckets.filter(user_id=request.user.id)
+        context = {
+            "buckets_list":buckets_list,
+        }
+        return render(request, "mypage.html", context)
 
 # 댓글
 def comments_create(request, bucket_id):
