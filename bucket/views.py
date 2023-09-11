6c04b04714f397c472827cb547f2cb56854048ee
USER\HOME <leekh9997@naver.com>
2023-09-11 12:09:14 +0900

    user>users로 바꿈

--- bucket/views.py
+++ bucket/views.py
@@ -14,18 +14,20 @@ def bucket(request):
         return render(request, "bucket.html", context)
 
 # 개인페이지
-@login_required(login_url='/user/login/')    
+@login_required(login_url='/users/login/')    
 @csrf_exempt 
 def mypage(request):
     if request.method == "GET":
         buckets = Bucket.objects.all()
-        buckets_list = buckets.filter(user_id=request.user.id)
+        buckets_list = buckets.filter(user_id=request.users.id)
         context = {
             "buckets_list":buckets_list,
         }
         return render(request, "mypage.html", context)
 
-# 댓글
+# 댓글생성
+@login_required(login_url='/users/login/')    
+@csrf_exempt
 def comments_create(request, bucket_id):
     if request.method == "POST":
         bucket = Bucket.objects.get(id=bucket_id)
@@ -36,6 +38,8 @@ def comments_create(request, bucket_id):
     else:
         return HttpResponse('Invalid request method', status=405)
 
+# 댓글삭제
+@csrf_exempt
 def comments_delete(request, bucket_id):
     if request.method == "POST":
         bucket = Bucket.objects.get(id=bucket_id)
