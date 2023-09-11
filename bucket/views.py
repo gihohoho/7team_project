688d7c8d7d101d0b56f65b045e6060abb1306653
Kim-Minjae9872 <141595332+Kim-Minjae9872@users.noreply.github.com>
2023-09-11 13:03:50 +0900

    변경사항 merge

--- bucket/views.py
+++ bucket/views.py
@@ -5,41 +5,49 @@ from django.contrib.auth.decorators import login_required
 from bucket.models import Bucket
 
 # 메인페이지
+
+
 def bucket(request):
     if request.method == "GET":
         buckets = Bucket.objects.all()
-        context ={
-            "buckets":buckets,
+        context = {
+            "buckets": buckets,
         }
         return render(request, "bucket/bucket.html", context)
 
 # 개인페이지
-@login_required(login_url='/users/login/')    
-@csrf_exempt 
+
+
+@login_required(login_url='/users/login/')
+@csrf_exempt
 def mypage(request):
     if request.method == "GET":
         buckets = Bucket.objects.all()
         buckets_list = buckets.filter(user_id=request.users.id)
         context = {
-            "buckets_list":buckets_list,
+            "buckets_list": buckets_list,
         }
         return render(request, "bucket/mypage.html", context)
-    
+
 # 새로만들기
-@login_required(login_url='/user/login/')
-@csrf_exempt    
+
+
+@login_required(login_url='/users/login/')
+@csrf_exempt
 def create(request):
     if request.method == "GET":
-       return render(request, "bucket/create.html")
+        return render(request, "bucket/create.html")
     elif request.method == "POST":
         Bucket.objects.create(
-            title = request.POST["title"],
-            content = request.POST["content"],
-            user = request.user,
+            title=request.POST["title"],
+            content=request.POST["content"],
+            user=request.user,
         )
     return redirect("/bucket/")
 
 # 댓글
+
+
 def comments_create(request, bucket_id):
     if request.method == "POST":
         bucket = Bucket.objects.get(id=bucket_id)
@@ -49,6 +57,7 @@ def comments_create(request, bucket_id):
         pass
     else:
         return HttpResponse('Invalid request method', status=405)
+
 
 def comments_delete(request, bucket_id):
     if request.method == "POST":
