dbfbdf7a4baffe868b9267db08ffc7208eb8bea7
USER\HOME <leekh9997@naver.com>
2023-09-11 17:15:25 +0900

    댓글쓰기

--- bucket/urls.py
+++ bucket/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bucket),
-    path('mypage/', views.mypage),
+    path('', views.bucket),  # 전체 글 리스트
+    path('mypage/', views.mypage),  # 내 글 리스트
     path('create/', views.create),
-    path('<int:bucket_id>/', views.detail),
-    path('<int:bucket_id>/comments_create/', views.comments_create),
-    path('<int:bucket_id>/comments_delete/', views.comments_delete),
+    path('<int:bucket_id>/', views.detail),  # 게시물 1개 보기
+    path('comments_create/<int:bucket_id>/', views.comments_create),  # 댓글쓰기
+    path('comments_delete/<int:bucket_id>/', views.comments_delete),  # 댓글삭제
 ]
