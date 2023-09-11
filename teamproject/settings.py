26e475a2d2e3beee4d960a6218b02cb9b8978086
hjzheld <dusen7774@naver.com>
2023-09-11 10:28:14 +0900

    team7_project 추가/bootstrap4추가

--- teamproject/settings.py
+++ teamproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bucket',
     'users',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'teamproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'team7_project'/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
