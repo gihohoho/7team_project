984726d4c7b92b26902b2fe5877024b0166c9a17
USER\HOME <leekh9997@naver.com>
2023-09-13 15:38:48 +0900

    📝 좋아요 기능 추가

--- bucket/urls.py
+++ bucket/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
      path('<int:bucket_id>/', views.detail),  # 게시물 1개 보기
      path('comments_create/<int:bucket_id>/', views.comments_create),  # 댓글쓰기
      path('comments_delete/<int:bucket_id>/<int:comment_id>/', views.comments_delete),  # 댓글삭제
+     path('likes/<int:bucket_id>/', views.likes, name='likes'),
 ]
